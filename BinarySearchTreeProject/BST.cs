f6eac95a53205038efcd83bdd89b3289c4320281
wchandler2018 <36997950+wchandler2018@users.noreply.github.com>
2020-01-31 14:28:14 -0600

    created Insert method in the BTS class

---
+++ BinarySearchTreeProject/BST.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    class BST
+    {
+        public Node root;
+
+        public void Insert(int data)
+        {
+            Node node = new Node(data);
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                Node current = root;
+                while (true)
+                {
+                  
+                    if (data < current.data)
+                    {
+                        if (current.LeftNode == null)
+                        {
+                            //current.LeftNode = current;
+                            current = node;
+                            break;
+                        }
+                        else
+                        {
+                            current = current.LeftNode;
+                        }
+
+                    }
+                    else
+                    {
+                        if (current.RightNode == null)
+                        {
+                            //root.RightNode = current;
+                            current = node;
+                            break;
+                        }
+                        else
+                        {
+                            current = current.RightNode;
+                        }
+                    }
+                }  
+            }
+        }
+
+        public Boolean Search(int data)
+        {
+            Node node = new Node(data);
+            if (root == null)
+            {
+                return false;
+            }else
+            {
+                while (true)
+                {
+                    Node current = root;
+                    if (data < current.data)
+                    {
+                        if (current.LeftNode == null)
+                        {
+
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        if (current.RightNode == null)
+                        {
+
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ BinarySearchTreeProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BST bst = new BST();
+            bst.Insert(100);
+            bst.Insert(75);
+            bst.Insert(55);
+            bst.Insert(125);
+            bst.Insert(110);
+            bst.Insert(105);
+            bst.Insert(90);
+            bst.Insert(45);
+            bst.Search(105);
+        }
+    }
+}
